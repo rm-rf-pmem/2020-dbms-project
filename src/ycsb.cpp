8b97eda667097132f678deabe41d7eac9f6e8c84
fffengmjl <m781131011@outlook.com>
2020-06-22 22:27:51 +0800

    优化 ycsb

--- src/ycsb.cpp
+++ src/ycsb.cpp
@@ -46,11 +46,11 @@ void getFileList()
   string dirname;
   DIR *dp;
   struct dirent *dirp;
-  if ((dp = opendir(WORKLOAD)) == NULL)
+  if ((dp = opendir(WORKLOAD)) == NULL) // 打开文件夹失败
   {
     cout << "Can't open " << WORKLOAD << endl;
   }
-  while ((dirp = readdir(dp)) != NULL)
+  while ((dirp = readdir(dp)) != NULL) // 打开文件夹
   {
     if (regex_match(dirp->d_name, loadReg))
       loadFileList.push_back(dirp->d_name);
@@ -63,24 +63,28 @@ void getFileList()
   for (int i = 0; i < loadFileList.size(); i++)
   {
     files.push_back(*new txtInfo(loadFileList[i]));
-    cout << "[FIND FILE] " << files[i].getLoadName() << ' ' << files[i].getRunName() << endl;
+    cout << "[FIND FILE] " << files[i].getLoadName() << ' ' 
+         << files[i].getRunName() << endl;
   }
 
-  cout << endl
-       << endl;
-  testNum = loadFileList.size();
+  cout << endl << endl;
+
+  testNum = loadFileList.size(); // 文件数量
 }
 
 void loadFile(txtInfo file, PmEHash *db = nullptr)
 {
   fstream f;
   f.open(WORKLOAD + file.getLoadName(), ios::in);
+
+  // 开始标识栏
   cout << "[STATUS] start to init DB with " << file.getLoadName() << endl;
   cout << "---------------------------------------------------" << endl;
   string cmd = "";
   string data = "";
-  time_t start = clock();
-  long long dataNum = 0;
+  time_t start = clock(); // 计时开始
+  long long dataNum = 0; // 计算数据数量
+
   while (!f.eof())
   {
     cmd = data = "";
@@ -88,20 +92,27 @@ void loadFile(txtInfo file, PmEHash *db = nullptr)
     if (cmd == "")
       break;
     f >> data;
+
+    // 构建 kv 对
     string k = data.substr(0, 8);
     int v = atoi(data.substr(8).c_str());
     kv kv_pair;
     kv_pair.key = stringTo64(k);
     kv_pair.value = v;
+
     // 测试时注释掉
-    db->insert(kv_pair);
+    db->insert(kv_pair); //插入操作
+
     dataNum++;
-    if (dataNum % 10000 == 0)
+    if (dataNum % 10000 == 0) // 每1W次操作就进行一次输出
       cout << "[STATUS] has load " << dataNum << "/" << file.getDataSize() << " data" << endl;
   }
-  time_t end = clock();
+
+  time_t end = clock(); // 计时结束
+
+  // 结束标识栏
   cout << "[STATUS] Init DB finished, use time ";
-  printf("%.4lf", ((double)end - start) / CLOCKS_PER_SEC);
+  printf("%.5lf", ((double)end - start) / CLOCKS_PER_SEC); // 输出操作时间
   cout << "s" << endl << endl << endl;
   f.close();
 }
@@ -142,17 +153,17 @@ void runFile(txtInfo file, PmEHash *db = nullptr)
     uint64_t res = 0;
 
     // 暂时注释掉
-    if (cmd == "READ")
+    if (cmd == "READ") // 对应搜索
     {
       db->search(kv_pair.key, res);
       read++;
     }
-    else if (cmd == "INSERT")
+    else if (cmd == "INSERT") // 对应插入
     {
       db->insert(kv_pair);
       write++;
     }
-    else if (cmd == "UPDATE")
+    else if (cmd == "UPDATE") // 对应更新
     {
       db->update(kv_pair);
       write++;
@@ -161,34 +172,42 @@ void runFile(txtInfo file, PmEHash *db = nullptr)
     {
       cout << "[ERROR] INVALID COMMAND: " << cmd << endl;
     }
+
+    // 好像没见到删除
+
     cmdNum++;
-    if (cmdNum % 10000 == 0)
+    if (cmdNum % 10000 == 0) // 每1W次操作删除一次
       cout << "[STATUS] has run " << cmdNum << " command" << endl;
   }
   time_t end = clock(); // finish run
 
-
+  // 结束标识栏
   double spentTime = ((double)end - start) / CLOCKS_PER_SEC;
   cout << "[STATUS] Run DB finished "<< endl;
   cout << "---------------------------------------------------" << endl;
 
   cout << "use time ";
-  printf("%.4lfs\n", spentTime);
-  cout << "r/w = " << read << '/' << write << endl
-       << "speed: " << (long)(cmdNum / spentTime) << " commands/s"
+  printf("%.4lfs\n", spentTime);                                    // 操作时间
+  cout << "r/w = " << read << '/' << write << endl                  // 读写比
+       << "speed: " << (long)(cmdNum / spentTime) << " commands/s"  // 操作数/秒
        << endl << endl << endl;
   f.close();
 }
 
 int main()
 {
+  // 调试用的
   getFileList();
-  int test1 = 12345670;
-  // cout << stringTo64("85264696") << endl;
-  for (int i = 0; i < testNum; i++)
-  {
-    PmEHash *db = new PmEHash();
-    loadFile(files[i], db);
-    runFile(files[i], db);
-  }
+  PmEHash *db = new PmEHash();
+  loadFile(files[5], db);
+  runFile(files[5], db);
+  db->selfDestory();
+
+  // for (int i = 0; i < testNum; i++)
+  // {
+  //   PmEHash *db = new PmEHash();
+  //   loadFile(files[i], db);
+  //   runFile(files[i], db);
+  //   db->selfDestory();
+  // }
 }
