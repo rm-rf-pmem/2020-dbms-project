480636d9eddb7a17eae3ab2c830603aa8bea4fb2
fffengmjl <m781131011@outlook.com>
2020-06-22 15:23:40 +0800

    feat: finish laodFile()

--- src/ycsb.cpp
+++ src/ycsb.cpp
@@ -16,11 +16,12 @@ using namespace std;
 vector<fileCouple> files;
 vector<string> runFileList;
 vector<string> loadFileList;
+int testNum;
 const regex loadReg("(\\d+)w-rw-(\\d+)-(\\d+)-load.txt");
 const regex runReg("(\\d+)w-rw-(\\d+)-(\\d+)-run.txt");
 
 // 读取文件列表
-void loadFile()
+void getFileList()
 {
   files.clear();
   runFileList.clear();
@@ -43,13 +44,42 @@ void loadFile()
   }
   closedir(dp);
 
-  for (auto run : runFileList)
-    cout << "[RUN FILE] " << run << endl;
-  for (auto load : loadFileList)
-    cout << "[LOAD FILE] " << load << endl;
+  for (int i = 0; i < runFileList.size(); i++)
+  {
+    cout << "[FIND FILE] " << loadFileList[i] << ' ' << runFileList[i] << endl;
+  }
+
+  testNum = runFileList.size();
+}
+
+void loadFile(string loadFilePath, PmEHash *db = nullptr)
+{
+  fstream f;
+  f.open(WORKLOAD + loadFilePath, ios::in);
+  cout << "start to init DB with " << loadFilePath << endl;
+  cout << "---------------------------------------------------" << endl;
+  string cmd = "";
+  string data = "";
+  while (!f.eof())
+  {
+    f >> cmd;
+    f >> data;
+    string k = data.substr(0, 8);
+    int v = atoi(data.substr(8).c_str());
+    kv kv_pair;
+    kv_pair.key = (uint64_t)k.c_str();
+    kv_pair.value = v;
+    db->insert(kv_pair);
+  }
+  f.close();
 }
 
 int main()
 {
-  loadFile();
+  getFileList();
+  for (int i = 0; i < testNum; i++)
+  {
+    // PmEHash db;
+    loadFile(loadFileList[i]);
+  }
 }
