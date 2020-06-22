8ed9ebe98c57323760cdc067ed8d99027c6fe0b8
fffengmjl <m781131011@outlook.com>
2020-06-22 16:47:33 +0800

    feat(ycsb): 准备测试

--- src/ycsb.cpp
+++ src/ycsb.cpp
@@ -1,6 +1,5 @@
 // #include <pm_ehash.h>
 #include "../include/pm_ehash.h"
-#include "../include/data.h"
 #include <fstream>
 #include <iostream>
 #include <dirent.h>
@@ -8,22 +7,35 @@
 #include <vector>
 #include <string.h>
 #include <cstdio>
+#include <time.h>
 
 using namespace std;
 
 #define WORKLOAD "../workloads/"
 
-vector<fileCouple> files;
 vector<string> runFileList;
 vector<string> loadFileList;
 int testNum;
 const regex loadReg("(\\d+)w-rw-(\\d+)-(\\d+)-load.txt");
 const regex runReg("(\\d+)w-rw-(\\d+)-(\\d+)-run.txt");
 
+uint64_t stringTo64(string str)
+{
+  uint64_t res = 0;
+  for (int i = 0; i < str.length(); i++)
+  {
+    uint8_t temp = str[i];
+    res = res << 8;
+    res += temp;
+    // printf("%d\t%d\t%ld\n", str[i], temp, res);
+  }
+
+  return res;
+}
+
 // 读取文件列表
 void getFileList()
 {
-  files.clear();
   runFileList.clear();
   loadFileList.clear();
 
@@ -56,30 +68,93 @@ void loadFile(string loadFilePath, PmEHash *db = nullptr)
 {
   fstream f;
   f.open(WORKLOAD + loadFilePath, ios::in);
-  cout << "start to init DB with " << loadFilePath << endl;
+  cout << "[STATUS] start to init DB with " << loadFilePath << endl;
   cout << "---------------------------------------------------" << endl;
   string cmd = "";
   string data = "";
+  time_t start = clock();
+  long long dataNum = 0;
   while (!f.eof())
   {
+    cmd = data = "";
     f >> cmd;
     f >> data;
     string k = data.substr(0, 8);
     int v = atoi(data.substr(8).c_str());
     kv kv_pair;
-    kv_pair.key = (uint64_t)k.c_str();
+    kv_pair.key = stringTo64(k);
     kv_pair.value = v;
-    db->insert(kv_pair);
+    // 测试时注释掉
+    // db->insert(kv_pair);
+    dataNum++;
+    if (dataNum % 10000 == 0)
+      cout << "[STATUS] has load " << dataNum << " data" << endl;
   }
+  time_t end = clock();
+  cout << "[STATUS] Init DB finished, use time " << ((double)end - start) / CLOCKS_PER_SEC << "ms" << endl
+       << endl
+       << endl;
+  f.close();
+}
+
+void runFile(string runFilePath, PmEHash *db = nullptr)
+{
+  fstream f;
+  f.open(WORKLOAD + runFilePath, ios::in);
+  cout << "[STATUS] start to run DB with " << runFilePath << endl;
+  cout << "---------------------------------------------------" << endl;
+  string cmd = "";
+  string data = "";
+  time_t start = clock();
+  long long cmdNum = 0;
+
+  while (!f.eof())
+  {
+    cmd = data = "";
+    f >> cmd;
+    f >> data;
+    string k = data.substr(0, 8);
+    int v = atoi(data.substr(8).c_str());
+    kv kv_pair;
+    kv_pair.key = stringTo64(k);
+    kv_pair.value = v;
+    uint64_t res = 0;
+    if (cmd == "READ")
+    {
+      db->search(kv_pair.key, res);
+    }
+    else if (cmd == "INSERT")
+    {
+      db->insert(kv_pair);
+    }
+    else if (cmd == "UPDATE")
+    {
+      db->update(kv_pair);
+    }
+    else
+    {
+      cout << "[ERROR] INVALID COMMAND: " << cmd << endl;
+    }
+    cmdNum++;
+    if (cmdNum % 10000 == 0)
+      cout << "[STATUS] has run " << cmdNum << " command" << endl;
+  }
+  time_t end = clock();
+  cout << "[STATUS] Init DB finished, use time " << ((double)end - start) / CLOCKS_PER_SEC << "ms" << endl
+       << endl
+       << endl;
   f.close();
 }
 
 int main()
 {
   getFileList();
+  int test1 = 12345670;
+  // cout << stringTo64("85264696") << endl;
   for (int i = 0; i < testNum; i++)
   {
     // PmEHash db;
     loadFile(loadFileList[i]);
+    runFile(runFileList[i]);
   }
 }
